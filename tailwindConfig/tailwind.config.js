c7111c8dfa5f0c01cfa9f75abebd9969f440fa80
Peyman (ATH) Naderi <peymanath@gmail.com>
2023-01-20 05:12:58 -0800

    added header signin-one

--- tailwindConfig/tailwind.config.js
+++ tailwindConfig/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../components/**/*.{html,js}"],
+  content: ["../components/**/*.html"],
   theme: {
     extend: {
       fontFamily: {
